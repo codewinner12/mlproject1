e9acdd62aae92f5605dc993dfc97cb515725879f
codewinner29 <sreejana29work@gmail.com>
2025-05-01 21:55:37 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='alproject',
+    version='0.0.1',
+    author='Sreejana',
+    author_email='sreejana29work@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn'],
+)
